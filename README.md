5fc8f007d807f9787dca970d5689109509a426c3
Luis Parravicini <lparravi@gmail.com>
2022-08-15 08:59:05 -0300

    The https server script now receives the path as a parameter

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ WebXR template for Unity
 
 This is a Unity project created with Unity 2022.1.9f1 and with the modifications described in the unity-webxr-export's [Getting Started](https://github.com/De-Panther/unity-webxr-export/blob/master/Documentation/Getting-Started.md) doc, to have a starter template for a webxr project.
 
-The resulting webgl build needs to run on a https server, if you want to quickly test it without the need of installing a proper http server, you can use the script `https-server` (it was tested on Linux, it should run on macOS also). Run it in the directory where the built game is.
+The resulting webgl build needs to run on a https server, if you want to quickly test it without the need of installing a proper http server, you can use the script `https-server` (it was tested on Linux, it should run on macOS also). You need to pass the path where the game is build as a parameter: `python https-server where-the-game-is`
 
 
 ## Compression
--- https-server.py
+++ https-server.py
@@ -1,11 +1,24 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
-import ssl, os
+import ssl, os, sys
+from pathlib import Path
 
 #
-# From https://gist.github.com/dergachev/7028596?permalink_comment_id=3720350#gistcomment-3720350
+# Based on https://gist.github.com/dergachev/7028596?permalink_comment_id=3720350#gistcomment-3720350
 #
 
-os.system("openssl req -nodes -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -subj '/CN=mylocalhost'")
+
+if len(sys.argv) < 2:
+    print(f'usage: {sys.argv[0]} <path-where-game-is>')
+    exit(1)
+
+path = sys.argv[1]
+
+os.chdir(path)
+
+cert_fname = 'key.pem'
+if not Path(cert_fname).exists():
+    os.system(f"openssl req -nodes -x509 -newkey rsa:4096 -keyout {cert_fname} -out cert.pem -days 365 -subj '/CN=mylocalhost'")
+
 port = 4443
 httpd = HTTPServer(('0.0.0.0', port), SimpleHTTPRequestHandler)
 httpd.socket = ssl.wrap_socket(httpd.socket, keyfile='key.pem', certfile="cert.pem", server_side=True)
